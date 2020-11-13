09ed0ef1a2bb70f114e5151382680ea775d1fb75
lucas Trevizan <lucasevizan@hotmail.com>
2020-11-13 14:51:05 -0300

    Correcao camada basicSecurity

--- blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -31,7 +31,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
