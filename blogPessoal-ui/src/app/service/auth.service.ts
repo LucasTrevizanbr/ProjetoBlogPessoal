3a73f7fb5cb62f5289f0970a938f7fa9a0222265
lucas Trevizan <lucasevizan@hotmail.com>
2020-12-04 14:11:15 -0300

    Consumindo a api login, cadastro e criando  navbar sair e logar

--- blogPessoal-ui/src/app/service/auth.service.ts
+++ blogPessoal-ui/src/app/service/auth.service.ts
@@ -19,4 +19,27 @@ export class AuthService {
     return this.http.post<User>('http://localhost:9000/usuarios/cadastrar', user)
 
   }
+
+  btnSair(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if(token != null){
+      ok=true
+    }
+
+    return ok
+  }
+
+  btnLogin(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if(token == null){
+      ok=true
+    }
+
+    return ok
+  }
+  
 }
